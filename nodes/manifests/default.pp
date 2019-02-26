9e8bd086198116f1e2e3e6eca869f8e2f9c752ae
Mo Rawi <mo.rawi@reedbusiness.com>
2019-02-26 17:46:56 +0000

    committing the combined set of modules

---
+++ nodes/manifests/default.pp
@@ -0,0 +1,11 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include nodes::default
+class nodes::default {
+  node 'default' {
+   class {role::rolnginx:}
+  }
+}
